3270808f2342a38c99c4345a7abb32980d84fedf
agSuperCoder123 <alex@ahoy.ooo>
2023-11-16 10:59:10 -0500

    added basic song title + artist name placeholder

--- superahoy/app/(tabs)/two.tsx
+++ superahoy/app/(tabs)/two.tsx
@@ -7,6 +7,8 @@ export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Now Playing</Text>
+      <Text>Song Title</Text>
+      <Text>Artist Name</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
