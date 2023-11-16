324ca14ded4f618a3935b731f34409bcea41ac64
agSuperCoder123 <alex@ahoy.ooo>
2023-11-16 11:04:18 -0500

    added 2 more tsx - 3 and 4 - which are being used for song lists and the song artist

--- superahoy/app/(tabs)/_layout.tsx
+++ superahoy/app/(tabs)/_layout.tsx
@@ -4,6 +4,8 @@ import { Pressable, useColorScheme } from 'react-native';
 
 import Colors from '../../constants/Colors';
 
+import TabThreeScreen from './three';
+
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
@@ -11,7 +13,7 @@ function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return <FontAwesome size={24} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
---
+++ superahoy/app/(tabs)/three.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../../components/EditScreenInfo';
+import { Text, View } from '../../components/Themed';
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>List of Songs</Text>
+      <Text>This is where the Song Title Will Dynamically Generate</Text>
+      <Text>Artist Name</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="app/(tabs)/two.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
